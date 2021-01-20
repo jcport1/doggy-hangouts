5b8f09e6b729729337b78d7528c688ff7bcc2b63
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-01-19 22:07:20 -0500

    Adds create, show action,has_secure_password, and user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,28 @@ class UsersController < ApplicationController
 
     def create
 
+        @user = User.create(user_params)
+
+      
+
+        if @user.save
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else 
+            render :new 
+        end
 
     end
 
     def show 
 
+        @user = User.find(params[:id])
+
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :name)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
     has_many :requests
     has_many :answered_listings, through: :requests, source: :listing
     has_many :authored_listings, foreign_key: :author_id, class_name: "Listing"
+    has_secure_password
 
 end
