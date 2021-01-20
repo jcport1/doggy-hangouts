f30b9813048adad3ed7d19e167ce479ecda3e904
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-01-20 11:28:13 -0500

    Builds actions on Listing Controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -8,10 +8,27 @@ class ListingsController < ApplicationController
 
     def new
 
+        @listing = Listing.new 
+
     end
     
     def create
 
+        @listing = Listing.create(listing_params)
+
+            if @listing.save
+                #redirect to a homepage or dashboard
+
+                redirect_to listings_path 
+
+            else 
+                render :new 
+            end
+
+    end
+
+    def show 
+        @listing = Listing.find(params[:id])
     end
     
     def edit 
@@ -20,5 +37,12 @@ class ListingsController < ApplicationController
 
     def update 
         
+    end
+
+    private
+    
+    #belongs to author, pet
+    def listing_params 
+        params.require(:listing).permits(:title, :content)
     end
 end
