37d131cbf260094f6f48442314ef2af5aabb8233
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-01-20 19:08:03 -0500

    Adds Login, Login required, and current_user helpers to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+
+    def login_required
+
+        if !logged_in? 
+            redirect_to '/login', notice => "Login is required to see this page"
+        end
+
+    end
+
+    def logged_in?
+
+        #!! -> converts method to a boolean 
+        !!current_user
+    end
+
+    def current_user 
+        if sessions[:user_id]
+            @current_user = User.find_by(sessions[:user_id])
+            @current_user 
+        else 
+            false 
+        end
+    end
 end
