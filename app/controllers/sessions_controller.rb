4894b08cd5a587a952a8437d7d764cbe7c253d85
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-01-20 19:21:33 -0500

    Revises skip_before_action syntax

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 
-    skip_before_action :login_required, only => [:welcome, :create]
+    skip_before_action :login_required, :only => [:welcome, :create]
 
     def welcome 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-    skip_before_action :login_required, only => [:new, :create]
+    skip_before_action :login_required, :only => [:new, :create]
 
     def new
 
