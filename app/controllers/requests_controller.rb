b94530f73c1eb5f490eab35f03a4ed425e2fece1
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-01-21 13:42:03 -0500

    Updates Request New Form and Request New Controllers

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,17 +6,30 @@ class RequestsController < ApplicationController
 
     def new
 
-        @request = Request.new 
+        if params[:listing_id] && @listing = Listing.find_by_id(params[:listing_id])
 
+            @request = @listing.requests.build
+
+            
+
+        else 
+
+            @error = "Listing does not exist" if params[:listing_id]
+            @request = Request.new 
+
+        end
+
+       
     end
 
     def create
 
         @request = Request.create(request_params)
         @request.user_id = session[:user_id]
-        @request.listing_id = 
-
+    
         if @request.save
+
+            flash[:notice] = "Your request was sent!"
 
             redirect_to "/listings"
 
@@ -39,6 +52,6 @@ class RequestsController < ApplicationController
     #belongs to user, pet, & listing 
 
     def request_params 
-        params.require(:request).permit(:message, :date, :accept, :pet_id)
+        params.require(:request).permit(:message, :date, :accept, :pet_id, :listing_id)
     end
 end
