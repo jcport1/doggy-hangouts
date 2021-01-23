76eb6ba28e0f64a80a2b1689bde7eee436b073d2
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-01-22 22:42:11 -0500

    Adds faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do 
+
+    user1 = User.create(username: email: Faker::Internet.email, password:"password" name: Faker::Name.name )
+    user2 = User.create(username: email: Faker::Internet.email password: "password" name: Faker::Name.name )
+
+    pet1 = Pet.create(name: Faker::Creature::Dog.name, age: size:, user_id: user1)
+    pet2 = Pet.create(name: age: size:, user_id: user2)
+
+end 
+
+2.times do
+
+    list1 = Listing.create(title: content: author_id: user1 pet_id: pet1 )
+    list2 = Listing.create(title: content: author_id: pet2 pet_id: pet2 )
+
+end 
+
+# Request.create(message: date: pet_id: listing_id:)
+
