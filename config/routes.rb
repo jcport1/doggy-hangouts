2736b350c25b28b13bce945a270dea1871360923
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-01-22 22:11:46 -0500

    Updates Request Show Page

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   end
   resources :requests
   resources :pets
+  resources :users do 
+    resources :requests, only: [:show]
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
