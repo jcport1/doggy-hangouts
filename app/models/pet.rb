b2d41d9b25c02d0448fffac3716d9817b1249087
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-01-23 23:43:22 -0500

    validates breed attribute for pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,7 +4,7 @@ class Pet < ApplicationRecord
     has_many :requests
     has_many :listings 
 
-    validates :name, :age, :size, presence: true 
+    validates :name, :age, :size, :breed, presence: true 
 
     validates :size, inclusion: { in: %w(small medium large), message: "%{value} is not a valid size"}
     # validates :age, inclusion: { in: %w(puppy young adult adult senior), message: "%{value} is not a valid age"}
